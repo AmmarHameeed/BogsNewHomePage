dbe0f3548cfbd44e07840adcdec9eca2912fa0b0
Ammar Hameed <ammarro444@gmail.com>
2022-07-21 02:36:30 +0500

    June Updates
    June Updates:
    US module,
    CA module,
    Alternative module screen

--- temp/scripts/App.js
+++ temp/scripts/App.js
@@ -1317,3 +1317,39 @@ $(document).on(
     window.featuredSliderUnique.slideNext();
   }
 );
+var featuredSliderUniqueJune = new Swiper(".feature-product__june", {
+  spaceBetween: 15,
+  slidesPerView: "auto",
+  loop: true,
+  centeredSlides: true,
+  speed: 800,
+  pagination: {
+    el: ".feature_slider--pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      noSwiping: false,
+      speed: 500,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(document).on(
+  "click",
+  ".feature-product__june .feature_slider__slide.swiper-slide-prev",
+  function () {
+    window.featuredSliderUniqueJune.slidePrev();
+  }
+);
+$(document).on(
+  "click",
+  ".feature-product__june .feature_slider__slide.swiper-slide-next",
+  function () {
+    window.featuredSliderUniqueJune.slideNext();
+  }
+);
